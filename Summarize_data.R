7f3465a56ab9d217863703ab9fc15a75e2644f03
jarne2703 <jarne.vandongen@student.ucll.be>
2024-11-27 06:31:15 +0000

    Added Summary of data

---
+++ Summarize_data.R
@@ -0,0 +1,10 @@
+summary_tidy_movie <- tidy_movie %>%
+  group_by(movie_genre) %>%       # Group by genre
+  summarize(
+    movie_count = n(),      
+    avg_rating = mean(imdb_rating, na.rm = TRUE), 
+    .groups = "drop"        
+  )
+
+
+print(summary_tidy_movie)
